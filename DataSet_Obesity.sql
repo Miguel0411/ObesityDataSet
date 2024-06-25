35d1ea90e87b95dee9c398fc871bdf094ea6ab61
Miguel Albitres Luna <81734329+Miguel0411@users.noreply.github.com>
2024-06-25 09:53:32 -0500

    Update and rename Intro.sql to DataSet_Obesity.sql

---
+++ DataSet_Obesity.sql
@@ -0,0 +1,35 @@
+DATASET ObesityDataSet 
+La base de datos que se utiliza es Obesity Dataset, obtenido de Kaggle. 
+Los datos consisten en la estimación de los niveles de obesidad en personas de los países de México, Perú y Colombia,
+con edades entre 14 y 61 años y diversos hábitos alimentarios y condición física. Los datos fueron recolectados mediante 
+una plataforma web con una encuesta donde usuarios anónimos respondieron cada uno, luego se procesó la información obteniendo 
+17 atributos y 2111 registros.
+  
+Los atributos relacionados con los hábitos alimentarios son:
+-Consumo frecuente de alimentos ricos en calorías (FAVC), 
+-Frecuencia de consumo de vegetales (FCVC), 
+-Número de comidas principales (NCP), 
+-Consumo de alimentos entre comidas (CAEC), 
+-Consumo de agua diario (CH20 ), 
+-Consumo de alcohol (CALC). 
+  
+Los atributos relacionados con la condición física son: 
+-Monitoreo del consumo de calorías (SCC),
+-Frecuencia de actividad física (FAF), 
+-Tiempo de uso de dispositivos tecnológicos (TUE), 
+-Transporte utilizado (MTRANS),
+
+variables obtenidas:
+Sexo, Edad, Talla y Peso.
+
+Los valores de NObesidad son:
+•Bajo Peso Menos de 18.5
+•Normal 18.5 a 24.9
+•Sobrepeso 25.0 a 29.9
+•Obesidad I 30.0 a 34.9
+•Obesidad II 35.0 a 39.9
+•Obesidad III Mayor a 40
+  
+Los datos contienen datos numéricos y datos continuos, por lo que pueden usarse para análisis basados
+en algoritmos de clasificación, predicción, segmentación y asociación.*/
+
