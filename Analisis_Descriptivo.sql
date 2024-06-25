ba4e3e67435e343e764170ee9eef67cc4f7528c6
Miguel Albitres Luna <81734329+Miguel0411@users.noreply.github.com>
2024-06-25 11:05:30 -0500

    Add files via upload
    Análisis Descriptivo

---
+++ Analisis_Descriptivo.sql
@@ -0,0 +1,36 @@
+/*Análisis Descriptivo
+Distribución de Género:
+	Número de participantes por género.
+	Proporción de género en la muestra.
+Distribución de Edad:
+	Media, mediana, moda de la edad y dispersión (desviación estándar, rango).
+	Distribución de la edad por grupos etarios,un histograma (por ejemplo, adolescentes, adultos jóvenes, adultos, adultos mayores).
+Altura y Peso:
+	Calcular la media, mediana y desviación estándar de la altura y el peso.
+	Generar gráficos de dispersión para visualizar la relación entre altura y peso.
+Índice de Masa Corporal (IMC):
+	Calcular el IMC para cada participante utilizando la fórmula: IMC = Peso (kg) / (Altura (m))^2.
+	Clasificar a los participantes en categorías de IMC (Bajo Peso, Normal, Sobrepeso, Obesidad I, Obesidad II, Obesidad III).*/
+
+--Analisis Descriptivo
+--Tabla Completa
+select*from ObesityDataSet
+
+--Distribución de Género y Proporción:
+--	Número de participantes por género
+select	count(*) AS Cantidad, 
+		Genero,
+		(COUNT(*) * 1.0 / (SELECT COUNT(*) FROM ObesityDataSet)) AS Proporcion --cant. entre el total
+FROM ObesityDataSet
+GROUP BY Genero
+
+-- Distribución de Edad:
+	Media, mediana, moda de la edad y dispersión (desviación estándar, rango).
+	Distribución de la edad por grupos etarios,un histograma (por ejemplo, adolescentes, adultos jóvenes, adultos, adultos mayores).
+
+
+
+
+
+
+
